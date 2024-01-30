9ac7699b17e88eedd2462636a18330dc0840aa3a
ulassbagli@gmail.com <ulassbagli@gmail.com>
2024-01-30 14:04:06 +0300

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from "./components/product/product.component";
+import { CategoryComponent } from "./components/category/category.component";
+import { NaviComponent } from "./components/navi/navi.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, ProductComponent, CategoryComponent, NaviComponent, CommonModule]
 })
 export class AppComponent {
   title = 'northwind';
+  user: string = "Ulaş Bağlı"
 }
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+  products:Product[] = [];
+
+}
